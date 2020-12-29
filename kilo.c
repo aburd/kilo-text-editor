197ba95d150a59ac84a4f9e8379ca8f64b259d3c
aburd <burdick.aaron@gmail.com>
2020-12-29 16:26:49 +0900

    fix fallback rows/cols

--- kilo.c
+++ kilo.c
@@ -62,10 +62,32 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+
+  buf[i] = '\0';
+
+  printf("\r\nbuf[2]: '%s'\r\n", &buf[2]);
+
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (1 || (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) || ws.ws_col == 0) {
+  if ((ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1) || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     return getCursorPosition(rows, cols);
   }
@@ -77,23 +99,8 @@ int getWindowSize(int *rows, int *cols) {
 void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
-  printf("width: %d height: %d", E.screenrows, E.screencols);
 }
 
-int getCursorPosition(int *rows, int *cols) {
-  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
-  printf("\r\n");
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1) {
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-  }
-  editorReadKey();
-  return -1;
-}
 
 /*** input ***/
 
@@ -112,7 +119,7 @@ void editorProcessKeypress() {
 /*** output ***/
 
 void editorDrawRows() {
-  for (int y = 0; y < E.screencols; y++)
+  for (int y = 0; y < E.screenrows; y++)
   {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
