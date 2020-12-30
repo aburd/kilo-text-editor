b73455133171ba21569c0df27e9d2c00a00c0034
aburd <burdick.aaron@gmail.com>
2020-12-31 01:34:47 +0900

    add fiel reading for open

--- kilo.c
+++ kilo.c
@@ -173,15 +173,27 @@ void initEditor() {
 
 /*** file i/o ***/
 
-void editorOpen() {
-  char *line = "Hello, world!";
-  ssize_t linelen = 13;
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("editorOpen");
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\r' ||
+                          line[linelen -1] == '\n'))
+      linelen--;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
 }
 
 
@@ -337,10 +349,13 @@ void editorRefreshScreen() {
 
 /*** init ***/
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-  editorOpen();
+  if (argc >=2) {
+    char *filename = argv[1];
+    editorOpen(filename);
+  }
 
   while (1) {
     editorRefreshScreen();
