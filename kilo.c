fd139789db35938679a33381e786c7df14429c83
aburd <burdick.aaron@gmail.com>
2020-12-31 00:53:54 +0900

    add delete key

--- kilo.c
+++ kilo.c
@@ -23,6 +23,7 @@ enum editorKey {
   PAGE_DOWN,
   HOME_KEY,
   END_KEY,
+  DEL_KEY,
 };
 
 /*** data ***/
@@ -86,6 +87,7 @@ int editorReadKey() {
         if (seq[2] == '~') {
           switch(seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
